86be3ee3f12eaec733e6a6dd5ada8e9c316911ab
Colin Regan <colin@reserve.ag>
2024-05-31 00:15:45 +0000

    Changes return message

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -16,7 +16,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         response = {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello c$',
+                message: 'hello canary',
             }),
         };
     } catch (err: unknown) {
